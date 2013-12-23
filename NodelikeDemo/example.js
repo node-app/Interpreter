6d30ff7ea4d782ce97f3de3a949a23eb02663e6a
Sam Rijs <sam@dev.local>
2013-12-23 20:56:50 +0100

    update example

--- NodelikeDemo/example.js
+++ NodelikeDemo/example.js
@@ -1,15 +1,16 @@
 log("hello world");
 
-global.Buffer = require("buffer").Buffer;
-
-try {
+module.exports = function () {
 
     var net = require('net');
+
     var server = net.createServer(function (c) {
         log('connect');
+        c.on("data", function (data) {
+            log("data: " + data.toString());
+        });
     });
+
     server.listen(5000);
 
-} catch(e) {
-    log(e.toString())
-}
+};
