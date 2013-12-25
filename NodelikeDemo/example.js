ba9ee6ef6e13c308ace2503eb244ed1dc641a64c
Sam Rijs <sam@dev.local>
2013-12-25 13:43:14 +0100

    update sample

--- NodelikeDemo/example.js
+++ NodelikeDemo/example.js
@@ -5,12 +5,17 @@ module.exports = function () {
     var net = require('net');
 
     var server = net.createServer(function (c) {
-        log('connect');
+        log('server connect');
         c.on("data", function (data) {
             log("data: " + data.toString());
         });
     });
 
     server.listen(5000);
+    
+    var con = net.createConnection({port:5000}, function () {
+        log('client connect');
+        con.write('hello world');
+    });
 
 };
